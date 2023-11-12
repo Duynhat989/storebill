5804374a0e226c46d1e7f8e6b410769a56ca28de
duynhat989 <vietduy989kc@gmail.com>
2023-11-12 13:53:53 +0700

    2

--- utils/db.js
+++ utils/db.js
@@ -10,10 +10,13 @@ require('dotenv').config();
 // module.exports = {
 //     dbConn
 // }
+const mysql2 = require('mysql2');
+// import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.NAME_DATABASE,  process.env.USER_DATABASE, process.env.PASS_DATABASE, {
   host: process.env.HOST_DATABASE,
+  dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
   dialect: 'mysql', // Loại cơ sở dữ liệu bạn đang sử dụng (postgres, mysql, sqlite, ...)
 });
 module.exports = {
